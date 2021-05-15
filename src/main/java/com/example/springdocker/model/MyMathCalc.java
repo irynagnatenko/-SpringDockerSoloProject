cf6e795901784fcc893a1cc9433e4e9e9b8d90b3
irynagnatenko <ira.gnatenko@gmail.com>
2021-05-15 22:41:14 +0200

    added methods to MyMathCalc

--- src/main/java/com/example/springdocker/model/MyMathCalc.java
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -1,7 +1,21 @@
-package com.example.springdocker.model;/**
+package com.example.springdocker.model;
+
+/**
  * Created by Iryna Gnatenko
  * Date 2021-05-15
  * Time 10:34 PM
  * Project springdocker
-*/ public class MyMathCalc {
+ */
+public class MyMathCalc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b){
+        return a / b;
+    }
 }
